70e46e58410a9e98ece02fc53059200268e0b9b5
farisrahman674 <farisrahman674@gmail.com>
2026-03-12 12:53:21 +0700

    fix: change code

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import { getDictionary } from "@/lib/getDictionary";
 import HomeClient from "./[locale]/HomeClient";
+import Navbar from "./[locale]/component/Navbar";
+import Footer from "./[locale]/component/MainFooter";
 
 export default async function Page() {
   const locale = "id";
   const dict = await getDictionary(locale);
 
-  return <HomeClient dict={dict} locale={locale} />;
+  return (
+    <>
+      <Navbar locale={locale} dict={dict} />
+
+      <HomeClient locale={locale} dict={dict} />
+
+      <Footer locale={locale} dict={dict} />
+
+      <footer className="bg-gray-900 text-white p-6 text-center">
+        © 2026 BCTI
+      </footer>
+    </>
+  );
 }
