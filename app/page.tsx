6ea734e3e84e4c62728c276d06143ad109a2493e
farisrahman674 <farisrahman674@gmail.com>
2026-03-27 10:36:44 +0700

    fix: change code in next.config.js and page root

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { permanentRedirect } from "next/navigation";
+// import { permanentRedirect } from "next/navigation";
 
-export default function Page() {
-  permanentRedirect("/id");
-}
+// export default function Page() {
+//   permanentRedirect("/id");
+// }
