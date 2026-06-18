873debca8e470a3b61aa18201b29ad2244001500
farisrahman <farisrahman674@gmail.com>
2026-06-18 09:53:23 +0700

    Testing 404 status

--- app/[locale]/product/machine/[slug]/page.tsx
+++ app/[locale]/product/machine/[slug]/page.tsx
@@ -3,42 +3,43 @@ import ProductDetail from "./ProductDetail";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { prisma } from "@/src/lib/prisma";
+import { cache } from "react";
 
+const getProductSlug = cache(async (slug: string) => {
+  return await prisma.product.findUnique({
+    where: { slug },
+    select: { id: true },
+  });
+});
 export async function generateMetadata({
   params,
 }: {
   params: { locale: "id" | "en"; slug: string };
 }): Promise<Metadata> {
+  const { slug } = await params;
+
+  const exists = await getProductSlug(slug);
+  if (!exists) {
+    notFound();
+  }
   return {
     alternates: {
       canonical: `/${params.locale}/product/machine/${params.slug}`,
     },
   };
 }
-export default function Page() {
-  notFound();
+
+export default async function Page({
+  params,
+}: {
+  params: { locale: "id" | "en"; slug: string };
+}) {
+  const { locale, slug } = await params;
+  const exists = await getProductSlug(slug);
+  if (!exists) {
+    notFound();
+  }
+  const dict = await getDictionary(locale);
+
+  return <ProductDetail dict={dict} locale={locale} />;
 }
-// export default async function Page({
-//   params,
-// }: {
-//   params: { locale: "id" | "en"; slug: string };
-// }) {
-//   const { locale, slug } = await params;
-//   const exists = await prisma.product.findUnique({
-//     where: {
-//       slug,
-//     },
-//     select: {
-//       id: true,
-//     },
-//   });
-
-//   if (!exists) {
-//     console.log("NOT FOUND TRIGGERED:", slug);
-//     throw new Error("TEST ERROR");
-//     notFound();
-//   }
-//   const dict = await getDictionary(locale);
-
-//   return <ProductDetail dict={dict} locale={locale} />;
-// }
