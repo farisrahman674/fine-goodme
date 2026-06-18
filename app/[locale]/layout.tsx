22ec632b8a4706d2f295f685dcf9bf84579fb300
farisrahman <farisrahman674@gmail.com>
2026-06-18 14:41:42 +0700

    Feat: Add google analytic and vercel analytic

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./component/Navbar";
 import Footer from "./component/MainFooter";
 import { getDictionary } from "@/lib/getDictionary";
 import { prisma } from "@/src/lib/prisma";
+import { Analytics } from "@vercel/analytics/react";
 export async function generateMetadata({
   params,
 }: {
@@ -74,6 +75,20 @@ export default async function RootLayout({
           </div>
         </footer>
         <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+  `}
+        </Script>
+        <Script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify([
@@ -93,6 +108,7 @@ export default async function RootLayout({
             ]),
           }}
         />
+        <Analytics />
       </body>
     </html>
   );
